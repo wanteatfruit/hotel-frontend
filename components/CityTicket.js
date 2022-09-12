9a3cd23163985ff7d20c8a759b6b3661ff9c2e76
Jesse An <feasiblechart@163.com>
2022-09-12 23:39:40 +0800

    improved ticket visual

--- components/CityTicket.js
+++ components/CityTicket.js
@@ -1,11 +1,11 @@
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import styles from "../styles/CityTicket.module.css";
-
+import FlightTakeoffIcon from "@mui/icons-material/FlightTakeoff";
 export function TicketSZ() {
   return (
     <div className={styles.ticket}>
       <p className={styles.ticketHeading}>深圳</p>
-      <ArrowForwardIcon className={styles.arrow} />
+      <FlightTakeoffIcon className={styles.arrow} />
       <div className={styles.pic_sz}> </div>
       <div className={styles.side}>开启你的旅程</div>
       <ul>
@@ -45,7 +45,7 @@ export function TicketGZ(){
       return (
         <div className={styles.ticket}>
           <p className={styles.ticketHeading}>广州</p>
-          <ArrowForwardIcon className={styles.arrow} />
+          <FlightTakeoffIcon className={styles.arrow} />
           <div className={styles.pic_gz}> </div>
           <div className={styles.side}>开启你的旅程</div>
           <ul>
@@ -85,7 +85,7 @@ export function TicketSH() {
   return (
     <div className={styles.ticket}>
       <p className={styles.ticketHeading}>上海</p>
-      <ArrowForwardIcon className={styles.arrow} />
+      <FlightTakeoffIcon className={styles.arrow} />
       <div className={styles.pic_sh}> </div>
       <div className={styles.side}>开启你的旅程</div>
       <ul>
@@ -124,7 +124,7 @@ export function TicketCQ() {
   return (
     <div className={styles.ticket}>
       <p className={styles.ticketHeading}>重庆</p>
-      <ArrowForwardIcon className={styles.arrow} />
+      <FlightTakeoffIcon className={styles.arrow} />
       <div className={styles.pic_cq}> </div>
       <div className={styles.side}>开启你的旅程</div>
       <ul>
