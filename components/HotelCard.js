10441edd486b25ec96364cfc717afbf19ff66e3f
wanteatfruit <feasiblechart@163.com>
2022-10-08 19:26:36 +0800

    加入custom scrollbar

--- components/HotelCard.js
+++ components/HotelCard.js
@@ -3,13 +3,15 @@ import styles from "../styles/HotelCard.module.css";
 import InfoIcon from '@mui/icons-material/Info';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
 import Image from "next/image";
+//hotelName为卡片上显示的字，imageSrc为图片地址，尽量在pexels.com上找图，
+//用其他网站的图片需要在next.config里设置域名
 export default function HotelCard({hotelName, imageSrc}) {
     return (
         <div className={styles.card}>
             <Image
                 src={imageSrc}
-                // width="400px"
-                // height="300px"
+                width="400px"
+                height="300px"
                 layout="fill"
                  />
             <div className={styles.info}>
@@ -20,7 +22,7 @@ export default function HotelCard({hotelName, imageSrc}) {
 
             <Typography sx={{ zIndex:2, position: 'absolute', bottom: 50,  color: 'white' }}
                 variant="h3">{hotelName}</Typography>
-            <Button variant="contained" sx={{ color: 'black', backgroundColor: 'antiquewhite', position: 'absolute', bottom: 0, zIndex: 2, borderRadius:0 }}>查看更多</Button>
+            <Button variant="contained" sx={{ color: 'black', backgroundColor: 'antiquewhite', position: 'absolute', bottom: -10, zIndex: 2, borderRadius:8 }}>查看更多</Button>
         </div>
     )
 }
--- pages/hotel.js
+++ pages/hotel.js
@@ -1,10 +1,17 @@
 import Layout from "../components/Layout";
-import { Box, Grid, Paper } from "@mui/material";
+import { Box, Grid, Paper, Tab } from "@mui/material";
 import Image from "next/image";
 import styles from "../styles/HotelPage.module.css";
 import { Stack } from "@mui/system";
 import HotelCard from "../components/HotelCard";
+import Slider from "react-slick";
+import React from "react";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
 export default function HotelPage() {
+  const [tabValue, setTabValue] = React.useState('0');
+  const handleTabChange =(event, newValue)=>{
+    setTabValue(newValue);
+  }
   return (
     <Layout>
       <Box
@@ -35,12 +42,36 @@ export default function HotelPage() {
           </div>
         </Box>
         <div className={styles.pickWrapper}>
-          <Box sx={{backgroundColor:'rebeccapurple'}}>
-            <Stack sx={{padding:2}} spacing={2} direction='row'>
-              <HotelCard hotelName={'分店1'} imageSrc={"https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=800"} />
-              <HotelCard hotelName={'分店2'} imageSrc={"https://images.pexels.com/photos/13794096/pexels-photo-13794096.jpeg?auto=compress&cs=tinysrgb&w=800"}/>
-                <HotelCard hotelName={'分店3'} />
-            </Stack>
+          <Box sx={{backgroundColor:'grey'}}>
+            <TabContext value={tabValue} >
+              <Box sx={{borderBottom:1,borderColor:'divider',paddingLeft:'20px'}}>
+                <TabList variant="fullWidth" sx={{fontSize:'xx-large'}} onChange={handleTabChange}>
+                  <Tab label="深圳" value="0"></Tab>
+                  <Tab label="广州" value="1"></Tab>
+                  <Tab label="重庆" value="2"></Tab>
+                  <Tab label="上海" value="3"></Tab>
+                </TabList>
+              </Box>
+              <TabPanel value="0">
+                <Stack sx={{ padding: 2,overflow:'scroll',justifyContent:{sm:'center',md:'flex-start'},alignItems:'center' }} spacing={2} direction={{ sm: 'column', md: 'row' }}>
+                  <div><HotelCard hotelName={'分店1'} imageSrc={"https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=800"} /></div>
+                  <div><HotelCard hotelName={'分店2'} imageSrc={"https://images.pexels.com/photos/13794096/pexels-photo-13794096.jpeg?auto=compress&cs=tinysrgb&w=800"} /></div>
+                  <div><HotelCard hotelName={'分店3'} imageSrc={'https://images.pexels.com/photos/5066935/pexels-photo-5066935.jpeg?auto=compress&cs=tinysrgb&w=800'} /></div>
+                  <div><HotelCard hotelName={'分店3'} imageSrc={'https://images.pexels.com/photos/5066935/pexels-photo-5066935.jpeg?auto=compress&cs=tinysrgb&w=800'} /></div>
+                  <div><HotelCard hotelName={'分店3'} imageSrc={'https://images.pexels.com/photos/5066935/pexels-photo-5066935.jpeg?auto=compress&cs=tinysrgb&w=800'} /></div>
+
+                </Stack>
+              </TabPanel>
+              <TabPanel value="1">
+                <HotelCard hotelName={'广州分店1'}></HotelCard>
+              </TabPanel>
+              <TabPanel value="2">
+                <HotelCard hotelName={'广州分店1'}></HotelCard>
+              </TabPanel>
+              <TabPanel value="3">
+                <HotelCard hotelName={'广州分店1'}></HotelCard>
+              </TabPanel>
+            </TabContext>
           </Box>
         </div>
       </Box>
