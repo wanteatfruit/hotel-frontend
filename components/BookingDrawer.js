5db2c09484bb352e625d12eb39b4785fd10ec7d2
wanteatfruit <feasiblechart@163.com>
2022-10-23 21:16:12 +0800

    添加订房

--- components/BookingDrawer.js
+++ components/BookingDrawer.js
@@ -1,6 +1,9 @@
-import { Drawer,Box,Stack,TextField,Tabs, Button, Tab ,Typography} from "@mui/material";
+import { Drawer, Box, Stack, TextField, Tabs, Button, Tab, Typography, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 import React from "react";
-export default function BookingDrawer({open, close}){
+import { branchHotels } from "../data";
+export default function BookingDrawer({ open, close }) {
+
+
     function TabPanel(props) {
         const { children, value, index, ...other } = props;
 
@@ -14,7 +17,17 @@ export default function BookingDrawer({open, close}){
             >
                 {value === index && (
                     <Box sx={{ p: 3 }}>
-                        <Typography>{children}</Typography>
+                        <List>
+                            {branchHotels.map((item) => (
+                                <ListItem disablePadding>
+                                    <ListItemButton onClick={() => setBookingOpen(open)}>
+                                        <ListItemText>
+                                            {item.name}
+                                        </ListItemText>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
+                        </List>
                     </Box>
                 )}
             </div>
@@ -30,27 +43,41 @@ export default function BookingDrawer({open, close}){
 
     const [bookingOpen, setBookingOpen] = React.useState(false);
     const [bookingCity, setBookingCity] = React.useState(0);
-    return(
-        <Drawer anchor="right" open={open} onClose={() => { setBookingOpen(false) }} sx={{ position: 'absolute', width: '70vw' }}>
-            {/* <Button onClick={()=>open=false}>asdad</Button> */}
-            <Box sx={{ width: '70vw' }}>
-                <Stack>
-                    <div style={{ backgroundColor: 'chocolate', height: '20vh', display: 'flex', alignItems: 'flex-end', justifyContent: 'center' }}>
-                        <TextField variant="standard" label="搜索目的地" sx={{ marginX: '100px', paddingBottom: '10px', width: '100%' }}></TextField>
-                    </div>
-                    <Tabs value={bookingCity} onChange={(event, newValue)=>setBookingCity(newValue)}>
-                        <Tab label="深圳"{...allyProps(0)} ></Tab>
-                        <Tab label="广州"{...allyProps(1)}></Tab>
-                    </Tabs>
-                    <TabPanel value={bookingCity} index={0}>
-                        深圳
-                    </TabPanel>
-                    <TabPanel value={bookingCity} index={1}>
-                        广州
-                    </TabPanel>
 
-                </Stack>
-            </Box>
-        </Drawer>
+    return (
+        <React.Fragment>
+            <Drawer id="select_city" anchor="right" open={open} sx={{ position: 'absolute', width: '70vw' }}>
+                {/* <Button onClick={()=>open=false}>asdad</Button> */}
+                <Box sx={{ width: '70vw' }}>
+                    <Stack>
+                        <div style={{ backgroundColor: 'chocolate', height: '20vh', display: 'flex', alignItems: 'flex-end' }}>
+                            <TextField variant="standard" label="搜索目的地" sx={{ marginX: '100px', paddingBottom: '10px', width: '100%' }}></TextField>
+                        </div>
+                        <Tabs value={bookingCity} onChange={(event, newValue) => setBookingCity(newValue)}>
+                            <Tab label="深圳"{...allyProps(0)} ></Tab>
+                            <Tab label="广州"{...allyProps(1)}></Tab>
+                        </Tabs>
+                        <TabPanel value={bookingCity} index={0}>
+                            深圳
+                        </TabPanel>
+                        <TabPanel value={bookingCity} index={1}>
+                            广州
+                        </TabPanel>
+
+                    </Stack>
+                </Box>
+            </Drawer>
+            <Drawer id="select_date" anchor="right" open={bookingOpen} sx={{ position: 'absolute', width: '70vw' }}>
+                {/* <Button onClick={()=>open=false}>asdad</Button> */}
+                <Box sx={{ width: '70vw' }}>
+                    <Stack>
+                        <div style={{ backgroundColor: 'darkgray', height: '20vh', display: 'flex', alignItems: 'flex-end' }}>
+
+                        </div>
+                    </Stack>
+                </Box>
+            </Drawer>
+        </React.Fragment>
+
     )
 }
