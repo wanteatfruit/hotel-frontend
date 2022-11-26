5002a716a298f3b02d9849d25ca3505060403892
wanteatfruit <feasiblechart@163.com>
2022-11-26 20:50:46 +0800

    修复显示bug

--- components/HotelCard.js
+++ components/HotelCard.js
@@ -3,10 +3,12 @@ import styles from "../styles/HotelCard.module.css";
 import InfoIcon from '@mui/icons-material/Info';
 import LocationCityIcon from '@mui/icons-material/LocationCity';
 import Image from "next/image";
+import Link from "next/link";
 //hotelName为卡片上显示的字，imageSrc为图片地址，尽量在pexels.com上找图，
 //用其他网站的图片需要在next.config里设置域名
 export default function HotelCard({hotelName, imageSrc, city,}) {
     return (
+        <Link href={`/hotels/${hotelName}`}>
         <div className={styles.card}>
             <Image
                 src={imageSrc}
@@ -21,7 +23,8 @@ export default function HotelCard({hotelName, imageSrc, city,}) {
 
             <Typography sx={{ zIndex:2, position: 'absolute', bottom: 50,  color: 'white' }}
                 variant="h3">{hotelName}</Typography>
-            <Button href={`/hotels/${hotelName}`} variant="contained" sx={{ color: 'black', backgroundColor: 'antiquewhite', position: 'absolute', bottom: 10, right:10, zIndex: 2, borderRadius:5 }}>查看更多</Button>
+            {/* <Button href={`/hotels/${hotelName}`} variant="contained" sx={{ color: 'black', backgroundColor: 'antiquewhite', position: 'absolute', bottom: 10, right:10, zIndex: 2, borderRadius:5 }}>查看更多</Button> */}
         </div>
+        </Link>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,11 @@ import Typography from "@mui/material/Typography";
 import styles from "../styles/Main.module.css";
 import Container from "@mui/material/Container";
 import Link from "next/link";
-import {createTheme, ThemeProvider} from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import NavBar from "../components/Navbar";
 
 import Footer from "../components/Footer";
-import {hotelImageUrl} from "../data";
+import { hotelImageUrl } from "../data";
 import Ticket, {
     TicketCQ,
     TicketGZ,
@@ -21,9 +21,9 @@ import Ticket, {
 } from "../components/CityTicket";
 
 import HotelCard from "../components/HotelCard";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 import Image from "next/future/image";
-import {Paper, SpeedDial, SpeedDialAction, SpeedDialIcon} from "@mui/material";
+import { Paper, SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
 import axios from "axios";
 
 import { useState } from "react";
@@ -51,7 +51,7 @@ export async function getStaticProps() {
 }
 
 
-export default function Home({hotel_list, room_list}) {
+export default function Home({ hotel_list, room_list }) {
     const router = useRouter()
     let _sessionKey = router.query['sessionKey'];
     let _username = router.query['username'];
@@ -66,10 +66,10 @@ export default function Home({hotel_list, room_list}) {
     const [isLogoutDialogOpen, setIsLogoutDialogOpen] = useState(false);
     const [chatDialogOpen, setChatDialogOpen] = useState(false)
     const jumpToCity = [
-        {name: '深圳', href: "#shenzhen"},
-        {name: '广州', href: "#guangzhou"},
-        {name: '上海', href: "#shanghai"},
-        {name: '重庆', href: "#chongqing"},
+        { name: '深圳', href: "#shenzhen" },
+        { name: '广州', href: "#guangzhou" },
+        { name: '上海', href: "#shanghai" },
+        { name: '重庆', href: "#chongqing" },
 
     ]
     const cardVariants = { //for hotel card anim
@@ -128,7 +128,7 @@ export default function Home({hotel_list, room_list}) {
 
             <div>
                 <NavBar userID={id} hotel_list={hotel_list} room_list={room_list} isLoggedIn={isLoggedIn}
-                        buttonsMode={0} clearLogInfo={clearLogInfo}/>
+                    buttonsMode={0} clearLogInfo={clearLogInfo} />
             </div>
             <main>
                 <Link
@@ -160,60 +160,60 @@ export default function Home({hotel_list, room_list}) {
                 </div>
                 <Stack>
 
-                    <Paper sx={{backgroundColor: 'antiquewhite'}} elevation={0}>
-                        <Stack paddingTop={14} paddingBottom={4} justifyContent='space-evenly'
-                               direction={{xs: 'column', sm: 'row'}} id="guangzhou">
-                            <motion.div viewport={{once: true}} style={{display: 'flex'}} initial={{opacity: 0}}
-                                        whileInView={{opacity: 1}} transition={{duration: 1, delay: 0.8}}>
+                    <Paper sx={{ backgroundColor: 'antiquewhite' }} elevation={0}>
+                        <Stack paddingTop={14} paddingBottom={10} justifyContent='space-evenly'
+                            direction={{ xs: 'column', sm: 'row' }} id="guangzhou">
+                            <motion.div viewport={{ once: true }} style={{ display: 'flex' }} initial={{ opacity: 0 }}
+                                whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.4 }}>
                                 <p className={styles.city}>
                                     广州
                                 </p>
                             </motion.div>
-                            <motion.div viewport={{once: true}} initial='offscreen' whileInView='onscreen'
-                                        variants={cardVariants}>
-                                <Stack paddingX={0} gap={10} direction={{xs: 'column', sm: 'row'}}>
+                            <motion.div viewport={{ once: true }} initial='offscreen' whileInView='onscreen'
+                                variants={cardVariants}>
+                                <Stack paddingX={0} gap={10} direction={{ xs: 'column', sm: 'row' }}>
                                     {hotel_list.map((item, index) => (item.cityname == "广州" &&
                                         <HotelCard hotelName={item.hotelname} key={item.hotelid}
-                                                   imageSrc={hotelImageUrl[index]}/>
+                                            imageSrc={hotelImageUrl[index]} />
                                     ))}
                                 </Stack>
                             </motion.div>
                         </Stack>
-                        <Stack paddingY={4} justifyContent='space-evenly' direction={{xs: 'column', sm: 'row'}}
-                               id="shanghai">
-                            <motion.div viewport={{once: true}} style={{display: 'flex'}} initial={{opacity: 0}}
-                                        whileInView={{opacity: 1}} transition={{duration: 1, delay: 0.8}}>
+                        <Stack paddingY={4} paddingBottom={10} justifyContent='space-evenly' direction={{ xs: 'column', sm: 'row' }}
+                            id="shanghai">
+                            <motion.div viewport={{ once: true }} style={{ display: 'flex' }} initial={{ opacity: 0 }}
+                                whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.4 }}>
                                 <p className={styles.city}>
                                     上海
                                 </p>
                                 {/* <Typography textAlign='end'  sx={{paddingBottom:{sm:0, xs:5}, writingMode: {sm:'vertical-lr',xs:'horizontal-tb'} }} variant="h1">上海</Typography> */}
                             </motion.div>
-                            <motion.div viewport={{once: true}} initial='offscreen' whileInView='onscreen'
-                                        variants={cardVariants}>
-                                <Stack paddingX={0} gap={10} direction={{xs: 'column', sm: 'row'}}>
+                            <motion.div viewport={{ once: true }} initial='offscreen' whileInView='onscreen'
+                                variants={cardVariants}>
+                                <Stack paddingX={0} gap={10} direction={{ xs: 'column', sm: 'row' }}>
                                     {hotel_list.map((item, index) => (item.cityname == "上海" &&
                                         <HotelCard hotelName={item.hotelname} key={item.hotelid}
-                                                   imageSrc={hotelImageUrl[index]}/>
+                                            imageSrc={hotelImageUrl[index]} />
                                     ))}
                                 </Stack>
                             </motion.div>
                         </Stack>
-                        <Stack paddingY={4} justifyContent='space-evenly' direction={{xs: 'column', sm: 'row'}}
-                               id="chongqing">
-                            <motion.div viewport={{once: true}} style={{display: 'flex'}} initial={{opacity: 0}}
-                                        whileInView={{opacity: 1}} transition={{duration: 1, delay: 0.8}}>
+                        <Stack paddingY={4} justifyContent='space-evenly' direction={{ xs: 'column', sm: 'row' }}
+                            id="chongqing">
+                            <motion.div viewport={{ once: true }} style={{ display: 'flex' }} initial={{ opacity: 0 }}
+                                whileInView={{ opacity: 1 }} transition={{ duration: 1, delay: 0.4 }}>
                                 <p className={styles.city}>
                                     重庆
                                 </p>
                             </motion.div>
-                            <motion.div viewport={{once: true}} initial='offscreen' whileInView='onscreen'
-                                        variants={cardVariants}>
+                            <motion.div viewport={{ once: true }} initial='offscreen' whileInView='onscreen'
+                                variants={cardVariants}>
 
-                                <Stack paddingX={0} gap={10} direction={{xs: 'column', sm: 'row'}}>
+                                <Stack paddingX={0} gap={10} direction={{ xs: 'column', sm: 'row' }}>
 
                                     {hotel_list.map((item, index) => (item.cityname == "重庆" &&
                                         <HotelCard hotelName={item.hotelname} key={item.hotelid}
-                                                   imageSrc={hotelImageUrl[index]}/>
+                                            imageSrc={hotelImageUrl[index]} />
                                     ))}
                                 </Stack>
                             </motion.div>
@@ -222,7 +222,7 @@ export default function Home({hotel_list, room_list}) {
 
                 </Stack>
             </main>
-            <Footer/>
+            <Footer />
         </ThemeProvider>
     );
 }
