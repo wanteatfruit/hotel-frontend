bb9201663df3af26197c32ec7cba686d98f22451
MichaelZhangJiahao <michaelzjh2002@gmail.com>
2022-11-30 19:39:41 +0800

    add roomImageUrl

--- data.js
+++ data.js
@@ -1,79 +1,90 @@
 export const pages = [
-  {name:"分店", link:"/hotels"},
-  { name: "入住", link: "/stay" },
+    {name: "分店", link: "/hotels"},
+    {name: "入住", link: "/stay"},
 ]; // 跳转到的界面名称
 export const settings = ["Account", "Logout"]; // 点用户图标后出来的选项
 export const login = ["Login"];
 export const cities = [
-  { name: "广州", id: 1 },
-  { name: "深圳", id: 2 },
-  { name: "上海", id: 3 },
-  { name: "重庆", id: 4 },
+    {name: "广州", id: 1},
+    {name: "深圳", id: 2},
+    {name: "上海", id: 3},
+    {name: "重庆", id: 4},
 ];
 
 export const branchHotels = [
-  { id: 1, city: "sz", name: "深圳文华东方酒店" },
-  { id: 2, city: "sz", name: "深圳丽思卡尔顿" },
-  { id: 3, city: "gz", name: "广州文华东方酒店" },
+    {id: 1, city: "sz", name: "深圳文华东方酒店"},
+    {id: 2, city: "sz", name: "深圳丽思卡尔顿"},
+    {id: 3, city: "gz", name: "广州文华东方酒店"},
 ];
 
 export const roomTypes = [
-  { id: 1, name: "山景房" },
-  { id: 2, name: "海景房" },
-  { id: 3, name: "总统套房" },
+    {id: 1, name: "山景房"},
+    {id: 2, name: "海景房"},
+    {id: 3, name: "总统套房"},
 ]
 
 export const orderColumns = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  { field: 'branch', headerName: '分店', width: 150 }
+    {field: 'id', headerName: 'ID', width: 90},
+    {field: 'branch', headerName: '分店', width: 150}
 ]
-export const orderRows = [{ id: 1, branch: "深圳文华东方酒店" }];
+export const orderRows = [{id: 1, branch: "深圳文华东方酒店"}];
 
 export const roomPageItem = []
 
+
+// idx从1开始，数组下标与roomtypeid对应
 export const roomImageUrl = [
-  "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?cs=srgb&dl=pexels-pixabay-164595.jpg&fm=jpg",
-  "https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/1579253/pexels-photo-1579253.jpeg?cs=srgb&dl=pexels-engin-akyurt-1579253.jpg&fm=jpg",
-  "https://images.pexels.com/photos/210265/pexels-photo-210265.jpeg?cs=srgb&dl=pexels-pixabay-210265.jpg&fm=jpg",
-  "https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?cs=srgb&dl=pexels-pixabay-210604.jpg&fm=jpg",
-  "https://images.pexels.com/photos/2029719/pexels-photo-2029719.jpeg?cs=srgb&dl=pexels-naim-benjelloun-2029719.jpg&fm=jpg",
-  "https://images.pexels.com/photos/3144580/pexels-photo-3144580.jpeg?cs=srgb&dl=pexels-jonathan-borba-3144580.jpg&fm=jpg",
-  "https://images.pexels.com/photos/276671/pexels-photo-276671.jpeg?cs=srgb&dl=pexels-pixabay-276671.jpg&fm=jpg",
-  "https://images.pexels.com/photos/1454806/pexels-photo-1454806.jpeg?cs=srgb&dl=pexels-jean-van-der-meulen-1454806.jpg&fm=jpg",
-  "https://images.pexels.com/photos/1571459/pexels-photo-1571459.jpeg?cs=srgb&dl=pexels-vecislavas-popa-1571459.jpg&fm=jpg",
-  "https://images.pexels.com/photos/189293/pexels-photo-189293.jpeg?cs=srgb&dl=pexels-donald-tong-189293.jpg&fm=jpg"
+    "",
+    "https://images.pexels.com/photos/164595/pexels-photo-164595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",    //1
+    "https://images.pexels.com/photos/210604/pexels-photo-210604.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/97083/pexels-photo-97083.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2467285/pexels-photo-2467285.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3754594/pexels-photo-3754594.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1454806/pexels-photo-1454806.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  //6
+    "https://images.pexels.com/photos/2029698/pexels-photo-2029698.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/271743/pexels-photo-271743.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2506990/pexels-photo-2506990.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3688261/pexels-photo-3688261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1329711/pexels-photo-1329711.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",  //11
+    "https://images.pexels.com/photos/3659683/pexels-photo-3659683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/279746/pexels-photo-279746.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3754595/pexels-photo-3754595.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13723007/pexels-photo-13723007.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1457842/pexels-photo-1457842.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",//16
+    "https://images.pexels.com/photos/210265/pexels-photo-210265.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/262048/pexels-photo-262048.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",//end
+    "https://images.pexels.com/photos/237371/pexels-photo-237371.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",//remaining URL
+    "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/1579253/pexels-photo-1579253.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 ]
 
 export const hotelImageUrl = [
-  "https://images.pexels.com/photos/1580112/pexels-photo-1580112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/2041556/pexels-photo-2041556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/1560065/pexels-photo-1560065.jpeg?cs=srgb&dl=pexels-bianca-1560065.jpg&fm=jpg",
-  "https://images.pexels.com/photos/3467153/pexels-photo-3467153.jpeg?cs=srgb&dl=pexels-rick-han-3467153.jpg&fm=jpg",
-  "https://images.pexels.com/photos/326691/pexels-photo-326691.jpeg?cs=srgb&dl=pexels-pixabay-326691.jpg&fm=jpg",
-  "https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?cs=srgb&dl=pexels-amar-saleem-70441.jpg&fm=jpg",
-  "https://images.pexels.com/photos/4023389/pexels-photo-4023389.jpeg?cs=srgb&dl=pexels-zachary-debottis-4023389.jpg&fm=jpg",
-  "https://images.pexels.com/photos/4004305/pexels-photo-4004305.jpeg?cs=srgb&dl=pexels-jeffrey-czum-4004305.jpg&fm=jpg",
+    "https://images.pexels.com/photos/1580112/pexels-photo-1580112.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/2041556/pexels-photo-2041556.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/1560065/pexels-photo-1560065.jpeg?cs=srgb&dl=pexels-bianca-1560065.jpg&fm=jpg",
+    "https://images.pexels.com/photos/3467153/pexels-photo-3467153.jpeg?cs=srgb&dl=pexels-rick-han-3467153.jpg&fm=jpg",
+    "https://images.pexels.com/photos/326691/pexels-photo-326691.jpeg?cs=srgb&dl=pexels-pixabay-326691.jpg&fm=jpg",
+    "https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?cs=srgb&dl=pexels-amar-saleem-70441.jpg&fm=jpg",
+    "https://images.pexels.com/photos/4023389/pexels-photo-4023389.jpeg?cs=srgb&dl=pexels-zachary-debottis-4023389.jpg&fm=jpg",
+    "https://images.pexels.com/photos/4004305/pexels-photo-4004305.jpeg?cs=srgb&dl=pexels-jeffrey-czum-4004305.jpg&fm=jpg",
 ]
 
 export const fullScreenHotelImageUrl = [
-  "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/2034335/pexels-photo-2034335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/1134176/pexels-photo-1134176.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/594077/pexels-photo-594077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-  "https://images.pexels.com/photos/2290753/pexels-photo-2290753.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    "https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/2034335/pexels-photo-2034335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/1134176/pexels-photo-1134176.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/594077/pexels-photo-594077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    "https://images.pexels.com/photos/2290753/pexels-photo-2290753.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
 ]
 
 // idx从1开始，数组下标与giftid对应
 export const giftImageUrl = [
-  "",
-  "https://images.pexels.com/photos/2912108/pexels-photo-2912108.jpeg?auto=compress&cs=tinysrgb&w=600",
-  "https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg",
-  "https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/13619261/pexels-photo-13619261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  "https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    "",
+    "https://images.pexels.com/photos/2912108/pexels-photo-2912108.jpeg?auto=compress&cs=tinysrgb&w=600",
+    "https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg",
+    "https://images.pexels.com/photos/1203803/pexels-photo-1203803.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1152077/pexels-photo-1152077.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13619261/pexels-photo-13619261.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/393047/pexels-photo-393047.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1112598/pexels-photo-1112598.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
 ]
